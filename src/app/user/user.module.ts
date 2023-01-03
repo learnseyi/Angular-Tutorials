ffb565bf0baec7108ed961ea00a84ff14894211f
learnseyi <69386196+learnseyi@users.noreply.github.com>
2023-01-02 19:59:29 -0600

    6 files modified, 1 added

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 import { StaffAuthComponent } from './staff-auth/staff-auth.component';
 import { SharedModule } from '../shared/shared.module';
-import {UserComponent} from './user/user.component';
-import {StaffComponent} from './staff/staff.component';
+import { UserComponent } from './user/user.component';
+import { StaffComponent } from './staff/staff.component';
 
 @NgModule({
-  imports: [CommonModule, SharedModule],
-  declarations: [AuthModalComponent, StaffAuthComponent,UserComponent,StaffComponent],
-  exports: [AuthModalComponent, StaffAuthComponent,UserComponent,StaffComponent]
+  imports: [CommonModule, SharedModule, ReactiveFormsModule],
+  declarations: [
+    AuthModalComponent,
+    StaffAuthComponent,
+    UserComponent,
+    StaffComponent,
+  ],
+  exports: [
+    AuthModalComponent,
+    StaffAuthComponent,
+    UserComponent,
+    StaffComponent,
+  ],
 })
 export class UserModule {}
--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
+  registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+    ]),
+  });
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
