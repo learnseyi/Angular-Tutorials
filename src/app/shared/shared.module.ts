e05376e2c6c442a4840f598db438c5df019358b6
learnseyi <69386196+learnseyi@users.noreply.github.com>
2023-01-05 00:11:44 -0600

    2 files modified, 3 added - refactoring forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,21 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabsComponent } from './tabs/tabs.component';
-
-
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ModalComponent,TabsContainerComponent,TabsComponent],
-  exports: [ModalComponent,TabsContainerComponent,TabsComponent],
+  declarations: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabsComponent,
+    InputComponent,
+  ],
+  exports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabsComponent,
+    InputComponent,
+  ],
 })
 export class SharedModule {}
