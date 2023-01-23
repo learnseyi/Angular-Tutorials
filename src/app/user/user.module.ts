5ad1cd3e94eb84a8501b014dcd48422eb0858870
learnseyi <69386196+learnseyi@users.noreply.github.com>
2023-01-22 22:46:05 -0600

    2 files modified, 3 deleted - deleted redundant components

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,24 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
-import { StaffAuthComponent } from './staff-auth/staff-auth.component';
 import { SharedModule } from '../shared/shared.module';
 import { UsersComponent } from './users/users.component';
 import { StaffComponent } from './staff/staff.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule, ReactiveFormsModule],
-  declarations: [
-    AuthModalComponent,
-    StaffAuthComponent,
-    UsersComponent,
-    StaffComponent
-  ],
-  exports: [
-    AuthModalComponent,
-    StaffAuthComponent,
-    UsersComponent,
-    StaffComponent,
-  ],
+  declarations: [AuthModalComponent, UsersComponent, StaffComponent],
+  exports: [AuthModalComponent, UsersComponent, StaffComponent],
 })
 export class UserModule {}
