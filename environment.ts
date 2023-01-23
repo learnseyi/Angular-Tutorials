8f4617699dce1658378a20253b736e20b262d8de
learnseyi <69386196+learnseyi@users.noreply.github.com>
2023-01-22 21:56:07 -0600

    3 files modified, 1 added - Configuring Firebase

---
+++ environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyADW86l7Owc3zjDZon0V0neAsn3ANl_k6c',
+    authDomain: 'clips-81532.firebaseapp.com',
+    projectId: 'clips-81532',
+    storageBucket: 'clips-81532.appspot.com',
+    messagingSenderId: '701727877272',
+    appId: '1:701727877272:web:e136a8064e4b5c79bf48ed',
+  },
+};
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {AngularFireModule} from '@angular/fire/compat';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+
+
+
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { UserModule } from './user/user.module';
+import { environment } from '../../environment';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, UserModule],
+  imports: [BrowserModule, FormsModule, UserModule,AngularFireModule.initializeApp(environment.firebaseConfig),AngularFireAuthModule],
   declarations: [AppComponent, NavComponent],
   bootstrap: [AppComponent],
 })
