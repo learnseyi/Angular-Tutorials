e701fc563df34facb90602b1366b509d664b1a19
learnseyi <69386196+learnseyi@users.noreply.github.com>
2023-02-19 21:09:57 -0600

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AngularFireModule } from '@angular/fire/compat'; // required to integrate firebase
+//import { AngularFireModule } from '@angular/fire/compat'; // required to integrate firebase
 // AngularFireAuthModule required to connect the firebase authentication service
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+// import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 // AngularFirestoreModule required to connect to the firestore service
 // import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
@@ -17,7 +17,7 @@ import { environment } from '../../environment';
     BrowserModule,
     FormsModule,
     UserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
+    // AngularFireModule.initializeApp(environment.firebaseConfig),
     // AngularFireAuthModule,
   ],
   declarations: [AppComponent, NavComponent],
